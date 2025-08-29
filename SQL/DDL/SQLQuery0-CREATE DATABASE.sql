744455c98c98ead2de98530a95e5233ad3bca7d0
User <User@DESKTOP-QHG18FL>
2025-08-29 16:26:33 +0300

    DDL

---
+++ SQL/DDL/SQLQuery0-CREATE DATABASE.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE PD_411_SQL
+ON			--параметр 'ON' задает свойства файла создаваемой Базы
+(
+	NAME		=	PD_411_SQL,	--Логическое имя файла
+	FILENAME	=	'D:\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\PD_411_SQL.mdf', --физическое имя файла
+	SIZE		=	8MB,
+	MAXSIZE		=	500MB,
+	FILEGROWTH	=	5MB
+)
+LOG ON		--этот параметр задает свойства файла жернала Базы данных
+(
+	NAME		=	PD_411_SQL_Log,
+	FILENAME	=	'D:\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\PD_411_SQL_log.ldf',
+	SIZE		=	8MB,
+	MAXSIZE		=	500MB,
+	FILEGROWTH	=	8MB
+);
+GO		--Применить
